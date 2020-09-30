ae2ccd82e8bfebe40801a24a23c8ea63457f7f2d
amahtab888 <71622826+amahtab888@users.noreply.github.com>
2020-09-30 12:17:27 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,10 @@
+from sklearn.naive_bayes import GaussianNB
+
+f=[[0,100],[0,120],[1,150],[1,180]]
+l=['orange','orange','apple','apple']
+
+clf=GaussianNB()
+
+trained=clf.fit(f,l)
+
+res=trained.predict([[1,100],[1,140],[0,180],[1,150],[1,100]])
